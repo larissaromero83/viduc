5bdfb7e978691af17d2d44722d226a1a9487eb6a
Ysosa <Yani_rsc@hotmail.com>
2019-04-16 08:27:20 -0400

    commit1

---
+++ Api Youtube/search.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * Library Requirements
+ *
+ * 1. Install composer (https://getcomposer.org)
+ * 2. On the command line, change to this directory (api-samples/php)
+ * 3. Require the google/apiclient library
+ *    $ composer require google/apiclient:~2.0
+ */
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$htmlBody = <<<END
+
+
+
+
+<form method="GET">
+  <div>
+    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+  </div>
+  <div>
+    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
+  </div>
+  <input type="submit" value="Search">
+</form>
+END;
+
+// This code will execute if the user entered a search query in the form
+// and submitted the form. Otherwise, the page displays the form above.
+if (isset($_GET['q']) && isset($_GET['maxResults'])) {
+  /*
+   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+   * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+   * Please ensure that you have enabled the YouTube Data API for your project.
+   */
+  $DEVELOPER_KEY = 'AIzaSyAcegEcjGVRThQ816PniqyLRsFD6WH4PHg';
+
+  $client = new Google_Client();
+  $client->setDeveloperKey($DEVELOPER_KEY);
+
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
+
+  $htmlBody = '';
+  try {
+
+    // Call the search.list method to retrieve results matching the specified
+    // query term.
+    $searchResponse = $youtube->search->listSearch('id,snippet', array(
+      'q' => $_GET['q'],
+      'maxResults' => $_GET['maxResults'],
+    ));
+    //<img src="URL DE LA IMAGEN" onmouseover="this.width=500;this.height=400;" onmouseout="this.width=200;this.height=150;" width="200" height="150" />
+    //'<iframe width="560" height="315" src="https://www.youtube.com/embed/%s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>﻿';
+    //<a class="thumbnail" href="#thumb"><img src="URL de la imagen" width="100px" height="80px" border="0" /><span><img src="URL de la imagen" /><br />Pie de foto</span></a>
+    //<iframe onmouseover="this.width=500;this.height=400;" onmouseout="this.width=200;this.height=150;" width="200" height="150" src="https://www.youtube.com/embed/%s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>﻿';
+  
+    
+    $videos = '';
+    $channels = '';
+    $playlists = '';
+    $direccion ='<iframe class="zoom" ; width="560" height="315" src="https://www.youtube.com/embed/%s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>﻿';
+
+    // Add each result to the appropriate list, and then display the lists of
+    // matching videos, channels, and playlists.
+    foreach ($searchResponse['items'] as $searchResult) {
+      switch ($searchResult['id']['kind']) {
+        case 'youtube#video':
+          $videos .= sprintf($direccion, $searchResult['id']['videoId']);
+          break;
+        case 'youtube#channel':
+          $channels .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
+          break;
+        case 'youtube#playlist':
+          $playlists .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
+          break;
+      }
+    }
+//<a class="thumbnail" href="#thumb"><img src="URL de la imagen" width="250px" height="150px" border="0" /><span><img src="URL de la imagen" /><br />Descripción para la foto.</span></a>
+    $htmlBody .= <<<END
+    <h3>Videos</h3>
+    <ul>$videos</ul>
+    <h3>Channels</h3>
+    <ul>$channels</ul>
+    <h3>Playlists</h3>
+    <ul>$playlists</ul>
+END;
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+}
+?>
+
+<!doctype html>
+<html>
+
+  <head>
+  <style type="text/css">
+	.zoom{
+		transition: 1.5s ease;
+ 		-moz-transition: 1.5s ease; /* Firefox */
+ 		-webkit-transition: 1.5s ease; /* Chrome - Safari */
+ 		-o-transition: 1.5s ease; /* Opera */
+     
+	}
+	.zoom:hover{
+		transform : scale(2);
+		-moz-transform : scale(2); /* Firefox */
+		-webkit-transform : scale(2); /* Chrome - Safari */
+		-o-transform : scale(2); /* Opera */
+		-ms-transform : scale(2); /* IE9 */
+    margin-left:auto;
+    margin-right:auto;
+   
+   
+}
+	}
+</style>
+    <title>YouTube Search</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+  </body>
+</html>

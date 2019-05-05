42c48587d9332ab0e9b70d8a7748277bc0ffd81a
unknown <gustavocalcaterra@gmail.com>
2019-05-05 15:26:21 -0400

    Read database params from env

--- guardar.php
+++ guardar.php
@@ -1,10 +1,15 @@
 <?php
-$usuario     = "root"; //en ste caso root por ser localhost
-$password    = ""; //contraseña por si tiene algun servicio de hosting 
-$servidor    = "localhost"; //localhost por lo del xampp
-$basededatos = "formulario1"; //nombre de la base de datos
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+error_log( "Hello, errors!" );
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$usuario     = getenv('MYSQL_USERNAME'); //en ste caso root por ser localhost
+$password    = getenv('MYSQL_PASSWORD'); //contraseña por si tiene algun servicio de hosting 
+$servidor    = getenv('MYSQL_SERVERNAME'); //localhost por lo del xampp
+$basededatos = getenv('MYSQL_DBNAME'); //nombre de la base de datos
 //por si hay errors de conexion un mensaje "Error con el servidor de la Base de datos".
-$conexion = mysqli_connect( $servidor, $usuario, "" ) or die( "Error con el servidor de la Base de datos" );
+$conexion = mysqli_connect( $servidor, $usuario, $password ) or die( "Error con el servidor de la Base de datos" );
 //por si hay errors de conexion un mensaje "Error al conectarse a la Base de datos".
 $db = mysqli_select_db( $conexion, $basededatos ) or die( "Error conexion al conectarse a la Base de datos" );
 //recuperar las variables
@@ -13,12 +18,14 @@ $sexo        = $_POST[ 'sexo' ]; //name="correo"
 $edad        = $_POST[ 'edad' ]; //name="mensaje" //name="mensaje"
 $preferencia = $_POST[ 'preferencia' ];
 //sentencia sql
-$sql         = "INSERT INTO datos VALUES ('','$nombre','$sexo','$edad','$preferencia')"; //manda a traer los valores de '$nombre','$correo','$mensaje'
+$sql         = "INSERT INTO datos (nombre, sexo, edad, preferencia) VALUES ('$nombre','$sexo','$edad','$preferencia')"; //manda a traer los valores de '$nombre','$correo','$mensaje'
 //ejecutamos la centencia de sql
 $ejecutar    = mysqli_query( $conexion, $sql );
 //verificacion de la ejecucioon
 if ( !$ejecutar ) {
-    echo "huvo algun error"; //si algo sale mal mandanos este mensaje
+    //echo "huvo algun error"; //si algo sale mal mandanos este mensaje
+    error_log("Error al ejecutar el insert: ");
+    error_log($ejecutar);
 } else {
     //Compruebo si hay algún resultado
     $row = mysqli_query( $conexion, "SELECT * FROM datos ORDER by id DESC limit 1 " ) or die( "problemas en consultas:" . mysqli_error() );
--- select.php
+++ select.php
@@ -1,9 +1,10 @@
 <?php
-$usuario     = "root"; //en ste caso root por ser localhost
-$password    = ""; //contraseña por si tiene algun servicio de hosting 
-$servidor    = "localhost"; //localhost por lo del xampp
-$basededatos = "formulario1"; //nombre de la base de datos
-$conexion = mysqli_connect( $servidor, $usuario, "" ) or die( "problemas al conectar server" );
+$usuario     = getenv('MYSQL_USERNAME'); //en ste caso root por ser localhost
+$password    = getenv('MYSQL_PASSWORD'); //contraseña por si tiene algun servicio de hosting
+$servidor    = getenv('MYSQL_SERVERNAME'); //localhost por lo del xampp
+$basededatos = getenv('MYSQL_DBNAME'); //nombre de la base de datos
+
+$conexion = mysqli_connect( $servidor, $usuario, $password ) or die( "problemas al conectar server" );
 mysqli_select_db( $conexion, $basededatos ) or die( "Error conexion al conectarse a la Base de datos" );
 $registro = mysqli_query( $conexion, "SELECT * FROM datos ORDER by id DESC limit 1 " ) or die( "problemas en consultas:" . mysqli_error() );
 while ( $reg = mysqli_fetch_array( $registro ) ) {

09eafe4aae890e6c76075d6e3ceca9dc5251228a
larissaromero83 <larissaromero83@gmail.com>
2019-05-29 15:31:54 -0400

    cambios tipo de letra

--- cocina.php
+++ cocina.php
@@ -6,7 +6,7 @@
 <!--<![endif]-->
 
 <head>
-   
+
     <!--- basic page needs
     ================================================== -->
     <meta charset="utf-8">
@@ -35,109 +35,109 @@
 
 </head>
 
-         <body id="top">
-            <!-- header
+<body id="top">
+    <!-- header
                ================================================== -->
-            <header class="s-header">
-               <div class="header-logo">
-                  <a class="site-logo" href="interes.html">
-                  <img src="images/logo.png" alt="Homepage">
-                  </a>
-               </div>
-            </header>
-            <!-- end s-header -->
-            <!-- contact
+    <header class="s-header">
+        <div class="header-logo">
+            <a class="site-logo" href="interes.html">
+                <img src="images/logo.png" alt="Homepage">
+            </a>
+        </div>
+    </header>
+    <!-- end s-header -->
+    <!-- contact
                ================================================== -->
-            <section id="contact" class="s-contact">
-               <div class="overlay"></div>
-               <div class="row section-header" data-aos="fade-up">
-                  <h1 class="display-2 display-2--light">Hola 
+    <section id="contact" class="s-contact">
+        <div class="overlay"></div>
+        <div class="row section-header" data-aos="fade-up">
+            <h1 class="display-2 display-2--light">Hola 
                      <?php
                         include("select.php");
                         ?>
                   </h1>
-                  <h3 class="subhead">Esto te puede interesar</h3>
-                  <h1 class="display-2 display-2--light">Varias recetas para aprender</h1>
-               </div>
-               </div>
-               <div class="row contact-contentes" data-aos="fade-up">
-                  <div class="contact-primary">
-                  <div style="text-align: left ;width:800px">
-					<input type="button" value=" Volver" onclick="location='index.html#contact'" />
-				</div>
-                     <div class="col-block service-item" data-aos="fade-up" id="columna1">
-                        <div class="service-text">
-                           <iframe width="500" height="300" align="left" src="https://www.youtube.com/embed/uY1cm-7PBes" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                           <h3 class="subhead">May May aprende a cocinar y hoy es Chef</h3>
-                           <br>
-                           <P ALIGN="justify">Pero lo que es excepcional en el caso de May es que para lograr convertirse en una chef de éxito no tuvo que asistir a mil y una clases de cocina, No, lo único que necesitó la chef Mahasrabphaisal fue YouTube. </p>
-                        </div>
-                     </div>
-                     <div id="columna2">
-                        <form action="search.php" method="GET">
-                           <div>
-                              <h3 class="subhead">Temas a buscar :</h3>
-                              <input type="search" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id=" q " name="q " placeholder="Temas Buscados " required>
+            <h3 class="subhead">Esto te puede interesar</h3>
+            <h1 class="display-2 display-2--light">Varias recetas para aprender</h1>
+        </div>
+        </div>
+        <div class="row contact-contentes" data-aos="fade-up">
+            <div class="contact-primary">
+                <div style="text-align: left ;width:800px">
+                    <input type="button" value=" Volver" onclick="location='index.html#contact'" />
+                </div>
+                <div class="col-block service-item" data-aos="fade-up" id="columna1">
+                    <div class="service-text">
+                        <iframe width="500" height="300" align="left" src="https://www.youtube.com/embed/uY1cm-7PBes" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h3 class="subhead">May May aprende a cocinar y hoy es Chef</h3>
+                        <br>
+                        <P ALIGN="justify">Pero lo que es excepcional en el caso de May es que para lograr convertirse en una chef de éxito no tuvo que asistir a mil y una clases de cocina, No, lo único que necesitó la chef Mahasrabphaisal fue YouTube. </p>
+                    </div>
+                </div>
+                <div id="columna2">
+                    <form action="search.php" method="GET">
+                        <div>
+                            <h3 class="subhead">Temas a buscar :</h3>
+                            <input type="search" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id=" q " name="q " placeholder="Temas Buscados " required>
                            </div>
                            <div >
-                              <h3 class="subhead"> Cantidad de Resultados:</h3>
-                              <input type="number" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id="maxResults" name="maxResults" min="1" max="50" step="1" value="10">
-                           </div>
-                           <div style="text-align: right ;width:300px">
-                              <input type="submit" value="Buscar">
-                           </div>
-                        </form>
-                     </div>
-                  </div>
-                  <div class="col-block service-item" data-aos="fade-up">
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/wStIus5jRbE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/nVVbJxqHvBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                  </div>
-                  <div class="col-block service-item" data-aos="fade-up">
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/f0bPm-6hWls" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/uga2DGux0_I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                  </div>
-                  <div class="col-block service-item" data-aos="fade-up">
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/MNNuEz1OHHk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                     <div class="service-text">
-                        <iframe width="450" height="250" src="https://www.youtube.com/embed/mm7btVGedpA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                     </div>
-                  </div>
-                 
-               </div>
-               <!-- end contact-primary -->
-               </div>
-               <!-- end contact-content -->
-            </section>
-            <!-- end s-contact -->
-            
-            <!-- preloader
-               ================================================== -->
-            <div id="preloader">
-               <div id="loader">
-                  <div class="line-scale-pulse-out">
-                     <div></div>
-                     <div></div>
-                     <div></div>
-                     <div></div>
-                     <div></div>
-                  </div>
-               </div>
+                           <h3 class="subhead"> Cantidad de Resultados:</h3><input type="number" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id="maxResults" name="maxResults" min="1" max="50" step="1" value="10">
+                        </div>
+                        <div style="text-align: right ;width:300px">
+                            <input type="submit" value="Buscar">
+                        </div>
+                    </form>
+                </div>
             </div>
-            <!-- Java Script
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/wStIus5jRbE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/nVVbJxqHvBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/f0bPm-6hWls" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/uga2DGux0_I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/MNNuEz1OHHk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/mm7btVGedpA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+
+        </div>
+        <!-- end contact-primary -->
+        </div>
+        <!-- end contact-content -->
+    </section>
+    <!-- end s-contact -->
+
+    <!-- preloader
                ================================================== -->
-            <script src="js/jquery-3.2.1.min.js"></script>
-            <script src="js/plugins.js"></script>
-            <script src="js/main.js"></script>
-         </body>
-      </html>
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <!-- Java Script
+               ================================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
--- costura.php
+++ costura.php
@@ -3,10 +3,9 @@
 <!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!-->
 <html class="no-js" lang="en">
-  <!--<![endif]-->
+<!--<![endif]-->
 
-
-  <head>
+<head>
 
     <!--- basic page needs
     ================================================== -->
@@ -34,150 +33,133 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-  </head>
+</head>
 
-  <body id="top">
-
-
+<body id="top">
 
     <!-- header
     ================================================== -->
     <header class="s-header">
 
-      <div class="header-logo">
-        <a class="site-logo" href="interes.html">
-          <img src="images/logo.png" alt="Homepage">
-        </a>
-      </div>
-
-
+        <div class="header-logo">
+            <a class="site-logo" href="interes.html">
+                <img src="images/logo.png" alt="Homepage">
+            </a>
+        </div>
 
     </header>
     <!-- end s-header -->
 
-
     <!-- contact
     ================================================== -->
     <section id="contact" class="s-contact">
-      <div class="overlay"></div>
+        <div class="overlay"></div>
 
+        <div class="row section-header" data-aos="fade-up">
 
-      <div class="row section-header" data-aos="fade-up">
+            <h1 class="display-2 display-2--light">Hola 
 
-        <h1 class="display-2 display-2--light">Hola 
-                
         <?php
         include("select.php");
         ?>
 
         </h1>
 
-        <h3 class="subhead">Esto te puede interesar</h3>
+            <h3 class="subhead">Esto te puede interesar</h3>
 
-        <h1 class="display-2 display-2--light">Tecnicas para aprender</h1>
-      </div>
-      </div>
+            <h1 class="display-2 display-2--light">Tecnicas para aprender</h1>
+        </div>
+        </div>
 
-      <div class="row contact-contentes" data-aos="fade-up">
+        <div class="row contact-contentes" data-aos="fade-up">
 
-        <div class="contact-primary">
-        <div style="text-align: left ;width:800px">
-					<input type="button" value=" Volver" onclick="location='index.html#contact'" />
-				</div>
+            <div class="contact-primary">
+                <div style="text-align: left ;width:800px">
+                    <input type="button" value=" Volver" onclick="location='index.html#contact'" />
+                </div>
 
-          <div class="col-block service-item" data-aos="fade-up" id="columna1">
-            <div class="service-text">
-              <iframe width="500" height="300" align="left" src="https://www.youtube.com/embed/zX8ERlu-TEc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              <h3 class="subhead">Aprende corte y confeccion</h3><br>Mira esto te puede ayudar a adquirir conocimientos y aprender un poco mas sobre este tema
+                <div class="col-block service-item" data-aos="fade-up" id="columna1">
+                    <div class="service-text">
+                        <iframe width="500" height="300" align="left" src="https://www.youtube.com/embed/zX8ERlu-TEc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <h3 class="subhead">Aprende corte y confeccion</h3>
+                        <br>Mira esto te puede ayudar a adquirir conocimientos y aprender un poco mas sobre este tema
 
-            </div>
-          </div>
+                    </div>
+                </div>
 
+                <div id="columna2">
 
-          <div id="columna2">
-
-            <form action="search.php" method="GET">
-              <div>
-                <h3 class="subhead">Temas a buscar :</h3> <input type="search" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id=" q " name=" q" placeholder="Temas Buscados "required>
+                    <form action="search.php" method="GET">
+                        <div>
+                            <h3 class="subhead">Temas a buscar :</h3>
+                            <input type="search" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id=" q " name=" q " placeholder="Temas Buscados "required>
                         </div>
                         <div >
                            <h3 class="subhead"> Cantidad de Resultados:</h3><input type="number" style=" border-style: solid; font-size:12pt; color: #f7f9f7; letter-spacing : 3px; id="maxResults" name="maxResults" min="1" max="50" step="1" value="10">
-                           </div>
+                        </div>
 
+                        <div style="text-align: right ;width:300px">
+                            <input type="submit" value="Buscar">
+                        </div>
+                    </form>
 
+                </div>
 
-              <div style="text-align: right ;width:300px">
-                <input type="submit" value="Buscar">
-              </div>
-            </form>
+            </div>
 
-          </div>
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/jCbMQsRq0CI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/5aj40M1wQ-c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
 
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/qclnreuFhEc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
-        </div>
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/LxoCOpxQ9Qw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
+            </div>
 
-        <div class="col-block service-item" data-aos="fade-up">
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/jCbMQsRq0CI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/7tkh8KrN4Go" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/5aj40M1wQ-c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-        </div>
+                <div class="service-text">
+                    <iframe width="450" height="250" src="https://www.youtube.com/embed/pT58_5gPL9A?start=14" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
 
-        <div class="col-block service-item" data-aos="fade-up">
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/qclnreuFhEc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/LxoCOpxQ9Qw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-
-        </div>
-
-        <div class="col-block service-item" data-aos="fade-up">
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube-nocookie.com/embed/7tkh8KrN4Go" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-
-          <div class="service-text">
-            <iframe width="450" height="250" src="https://www.youtube.com/embed/pT58_5gPL9A?start=14" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+            </div>
 
         </div>
-
-
-        
-
-
-
-      </div>
-      <!-- end contact-primary -->
-      </div>
-      <!-- end contact-content -->
+        <!-- end contact-primary -->
+        </div>
+        <!-- end contact-content -->
 
     </section>
     <!-- end s-contact -->
 
-
     <!-- preloader
     ================================================== -->
     <div id="preloader">
-      <div id="loader">
-        <div class="line-scale-pulse-out">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
-      </div>
     </div>
-
 
     <!-- Java Script
     ================================================== -->
@@ -185,6 +167,6 @@
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
 
-  </body>
+</body>
 
 </html>
